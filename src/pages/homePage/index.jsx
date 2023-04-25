85855bd13bdea5d1705f6fbafc77925ff43ee227
sammed-sankonatti <sammed.sankonatti18@gmail.com>
2023-04-26 00:47:16 +0530

    sample example of posts page with static data

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,11 +1,27 @@
-import { Box } from '@mui/material'
+import { Box, useMediaQuery } from '@mui/material'
 import NavBar from '../../components/NavBar'
 import React from 'react'
+import Posts from '../postsPage'
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   return (
     <Box>
       <NavBar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="center"
+      >
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <Posts />
+        </Box>
+      </Box>
     </Box>
   )
 }
---
+++ src/pages/postsPage/index.jsx
@@ -0,0 +1,78 @@
+import Post from '../../components/Post'
+import React from 'react'
+
+const Posts = () => {
+
+    const posts = [
+        {
+            _id : "1",
+            userId : "1",
+            firstName : "princess",
+            lastName : "sankonatti",
+            description : "my source of inspiration!",
+            location : "Bangalore",
+            picturePath : "https://cdn.pixabay.com/photo/2015/09/22/01/30/lights-951000_960_720.jpg",
+            userPicturePath:"https://cdn.pixabay.com/photo/2015/09/22/01/30/lights-951000_960_720.jpg",
+            likes : 100,
+            comments : 32
+        },
+        {
+            _id : "2",
+            userId : "2",
+            firstName : "Alex Xu",
+            lastName : "sankonatti",
+            description : "live the moment!",
+            location : "New york, USA",
+            picturePath : "https://c4.wallpaperflare.com/wallpaper/301/256/1013/dubai-city-lights-8k-uae-wallpaper-preview.jpg",
+            userPicturePath:"https://c4.wallpaperflare.com/wallpaper/301/256/1013/dubai-city-lights-8k-uae-wallpaper-preview.jpg",
+            likes : 500,
+            comments : 45
+        },
+        {
+            _id : "2",
+            userId : "2",
+            firstName : "Alex Xu",
+            lastName : "sankonatti",
+            description : "mind blowing view!",
+            location : "New york, USA",
+            picturePath : "https://c4.wallpaperflare.com/wallpaper/655/901/296/tower-block-uae-united-arab-emirates-cotton-candy-wallpaper-preview.jpg",
+            userPicturePath:"https://c4.wallpaperflare.com/wallpaper/301/256/1013/dubai-city-lights-8k-uae-wallpaper-preview.jpg",
+            likes : 1045,
+            comments : 312
+        }
+    ]
+
+  return (
+    <>
+      {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+    })=> (
+        <Post key={_id}
+          postId={_id}
+          postUserId={userId}
+          name={`${firstName}`}
+          description={description}
+          location={location}
+          picturePath={picturePath}
+          userPicturePath={userPicturePath}
+          likes={likes}
+          comments={comments} 
+
+        />
+        )
+      )}
+    </>
+  )
+}
+
+export default Posts
